709baf5f3acfc5db95dcc284c1085b3ebe1ffb86
DollySingh396 <dollysingh396@gmail.com>
2022-06-20 11:15:29 +0530

    Day 14

---
+++ Array/checkIfTwoArraysEqualOrNot.js
@@ -0,0 +1,36 @@
+function check(A,B,N){
+    // code here
+
+    // object to store the count of each array element
+    // here one element can occur more than one time
+    var obj = {};
+
+    // traverse A to get count of all elements of A
+    for( let i = 0; i < N; i++){
+        if(obj[A[i]]) obj[A[i]] += 1;
+        else obj[A[i]] = 1;
+    }
+
+    // traverse B , if same element is found decrease count from object
+    // otherwise for new element initilaise with -1
+    //
+    for( let i = 0; i < N; i++){
+        if(obj[B[i]]) obj[B[i]] -= 1;
+        else obj[A[i]] = -1;
+    }
+
+    // checking that each key of object has 0 value
+    // if yes return 1 else 0
+    var res = Object.keys(obj).every( key => obj[key] == 0) ? 1 : 0
+
+    return res;
+
+}
+/**
+ *  TC - O(N)
+ *  SC - O(N)
+ */
+var A = [1,2,5,4,0]
+var B = [2,4,5,0,1]
+var N = A.length;
+console.log(check(A,B,N));
